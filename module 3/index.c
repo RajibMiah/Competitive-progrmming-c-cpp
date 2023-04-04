fd372e910bde8c97595c4258437da275bfce4471
rajibmiah <ahmedrajib942@gmail.com>
2023-04-05 02:25:35 +0600

    continue statement

--- module 3/index.c
+++ module 3/index.c
@@ -1,17 +1,32 @@
 #include<stdio.h>
 
-// BREAK STATEMENT IN C
 int main(){
     int num , i;
     scanf("%d",&i);
     for(i = 1; i<= 10; i=i+1){
-        printf("%d\n", i);
+
+        // NOTE:: DO NOT DONE ANY KIND OF LOGICAL STATEMENT  ABOVE CONTINUE STATEMENT
         if(i == 5){
-            break;
+            continue;;
         }
+        printf("%d\n", i);
     }
     return 0;
 }
+
+
+// BREAK STATEMENT IN C
+// int main(){
+//     int num , i;
+//     scanf("%d",&i);
+//     for(i = 1; i<= 10; i=i+1){
+//         printf("%d\n", i);
+//         if(i == 5){
+//             break;
+//         }
+//     }
+//     return 0;
+// }
 
 // LOOP WITH EVEN ODD
 // int main(){
