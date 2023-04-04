3a4e53186e5e4fd3cd0015f56a28496cce29e6b2
rajibmiah <ahmedrajib942@gmail.com>
2023-04-05 02:21:07 +0600

    break statement in c

--- module 3/index.c
+++ module 3/index.c
@@ -1,14 +1,28 @@
 #include<stdio.h>
 
+// BREAK STATEMENT IN C
 int main(){
     int num , i;
-    scanf("%d", &num);
-    for (i=0 ; i<=num ; i++){
-        if(i % 2 == 0){
-            printf("%d--even\n", i);
-        }else{
-            printf("%d--odd\n", i);
+    scanf("%d",&i);
+    for(i = 1; i<= 10; i=i+1){
+        printf("%d\n", i);
+        if(i == 5){
+            break;
         }
     }
     return 0;
 }
+
+// LOOP WITH EVEN ODD
+// int main(){
+//     int num , i;
+//     scanf("%d", &num);
+//     for (i=0 ; i<=num ; i++){
+//         if(i % 2 == 0){
+//             printf("%d--even\n", i);
+//         }else{
+//             printf("%d--odd\n", i);
+//         }
+//     }
+//     return 0;
+// }
