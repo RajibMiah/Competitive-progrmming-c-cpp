23839f897234a79b0e51846df59946b8b0d24cd9
rajibmiah <ahmedrajib942@gmail.com>
2023-04-14 02:21:08 +0600

    assignment problem solve in hackerrank

---
+++ assignment_02/Can_You_Do_It.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+    int n;
+    scanf("%d" ,&n);
+    if(n <= 0){
+        for(int i= (n * -1) ; i >= -1 ; --i){
+            printf("%d ", i*-1);
+        }
+    }else{
+        for(int i=1 ; i<=n ; ++i){
+            printf("%d " , i);
+        }
+    }
+    return 0;
+}
