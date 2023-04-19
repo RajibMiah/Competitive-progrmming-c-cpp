134b58402ad16500c4343f8a324684285e096a9e
rajibmiah <ahmedrajib942@gmail.com>
2023-04-20 01:50:10 +0600

    EOF

---
+++ module 11.5/J_Count_Letters.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_LEN 1000000
+
+int main() {
+    char c;
+    int freq[26] = {0};
+    while(scanf("%c" , &c) != EOF){
+        freq[c- 'a']++;
+    }
+    for (int i = 0; i < 26; ++i) {
+        if (freq[i] != 0) {
+            printf("%c : %d\n", i + 'a', freq[i]);
+        }
+    }
+
+    return 0;
+}
