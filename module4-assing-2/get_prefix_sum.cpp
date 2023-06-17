3af0f6ee1d724aa55d93f348afd6931bc821fc68
rajibmiah <ahmedrajib942@gmail.com>
2023-06-17 14:24:08 +0600

    get prefix sum on hackerrank

--- module4-assing-2/get_prefix_sum.cpp
+++ module4-assing-2/get_prefix_sum.cpp
@@ -6,10 +6,6 @@ const int MAX_N = 1e5 + 5;
 const int MOD = 1e9 + 7;
 const double EPS = 1e-9;
 
-bool cmp(pair <int , int> a , pair < int ,int> b){
-    if(a.first != b.second) return a.first < b.second;
-    return a.first > b.second;
-}
 // Time limit	Operations (approximate)
 // 1 second     	10^7
 // 10 seconds	    10^8
@@ -17,9 +13,16 @@ bool cmp(pair <int , int> a , pair < int ,int> b){
 // 1 hour	        10^11
 int main(){
     int ttt ; cin >> ttt;
-    while (ttt--)
-    {
-        
+    vector<long long int> v1(ttt);
+    for(int i = 0 ; i< ttt ; ++i ){
+        cin >> v1[i];
     }
     
+    for(int i = 1 ; i < ttt ; ++i){
+        v1[i] += v1[i - 1];
+    }
+
+    for(int i = ttt - 1  ; i >= 0 ; --i){
+        cout << v1[i] << " ";
+    }
 }
